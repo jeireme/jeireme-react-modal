ce5e4014450a493053bd7d49ee75136485ad9ad5
Jérémie PIARD <jeremie.piard@yahoo.fr>
2022-11-12 01:09:11 +0300

    improved customization

--- dist/components/Modal.js
+++ dist/components/Modal.js
@@ -9,8 +9,18 @@ var _react = _interopRequireWildcard(require("react"));
 require("./Modal.css");
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+/**
+ * Displays a modal and an overlay on button click
+ *
+ * @component
+ * @example
+ * return (
+ *   <Modal openButttonText="Show modal" closeButtonText="OK" message="The modal message" />
+ * )
+ */
 function Modal(props) {
   const [modal, setModal] = (0, _react.useState)(false);
+  const defaultMessage = /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("h2", null, "Well Done!"), /*#__PURE__*/_react.default.createElement("p", null, "This is a simple modal you can customize whatever you want."));
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "container"
   }, /*#__PURE__*/_react.default.createElement("button", {
@@ -24,5 +34,10 @@ function Modal(props) {
     onClick: () => setModal(false)
   }, props.closeButtonText)));
 }
+Modal.defaultProps = {
+  openButttonText: "Show modal",
+  message: defaultMessage,
+  closeButtonText: "OK"
+};
 var _default = Modal;
 exports.default = _default;
